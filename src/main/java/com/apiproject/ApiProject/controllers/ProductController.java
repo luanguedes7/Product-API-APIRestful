0071e078bee4675f16cd4529612f5fb6012af1f6
Luan Guedes <lcrisley98@gmail.com>
2024-08-17 23:32:24 -0300

    Product get and Product put

--- src/main/java/com/apiproject/ApiProject/controllers/ProductController.java
+++ src/main/java/com/apiproject/ApiProject/controllers/ProductController.java
@@ -1,10 +1,17 @@
 package com.apiproject.ApiProject.controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +30,35 @@ public class ProductController {
 	@PostMapping("/products")
 	public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
 		var productModel = new ProductModel();
+		BeanUtils.copyProperties(productRecordDto, productModel);
+		return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+	}
+	
+	@GetMapping("/products")
+	public ResponseEntity<List<ProductModel>> getAllProducts() {
+		return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+	}
+	
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Object> getOneProduct(@PathVariable(value="id") UUID id) {
+		Optional<ProductModel> product = productRepository.findById(id);
+		
+		if (product.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found!");
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(product.get());
+	}
+	
+	@PutMapping("/products/{id}")
+	public ResponseEntity<Object> updateProduct(@PathVariable(value="id") UUID id, @RequestBody @Valid ProductRecordDto productRecordDto) {
+		Optional<ProductModel> product = productRepository.findById(id);
+		
+		if (product.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found!");
+		}
+		
+		var productModel = product.get();
 		BeanUtils.copyProperties(productRecordDto, productModel);
 		return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
 	}
